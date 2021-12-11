2ba2a08c5e5fb70b0166025da9d405ff138f956f
E2r7hN07Fl47 <43937145+E2r7hN07Fl47@users.noreply.github.com>
2021-12-12 02:27:06 +0300

    Initial commit

---
+++ app/src/main/java/ru/e2r7hn07fl47/botcontroller/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.e2r7hn07fl47.botcontroller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
